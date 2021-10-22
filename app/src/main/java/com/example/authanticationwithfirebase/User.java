4998b51bc2102557986692bcd0b0fe2c2dbad02c
colvan <yoniborisenco@gmail.com>
2021-10-22 13:36:46 +0300

    First Basic Authantication Register

---
+++ app/src/main/java/com/example/authanticationwithfirebase/User.java
@@ -0,0 +1,15 @@
+package com.example.authanticationwithfirebase;
+
+public class User {
+    public String fullname,email;
+
+    public User(){
+
+    }
+
+    public User(String fullname,String email){
+        this.fullname = fullname;
+        this.email = email;
+    }
+
+}
